ae544cc0fe208c3f6a9f113059a166464b31d8ff
Joseph Glass <glassjoseph@gmail.com>
2019-10-24 15:09:45 -0400

    Add files via upload

---
+++ resources/update_resources.rb
@@ -0,0 +1,35 @@
+# Ruby script to read a CSV and send API requests to update SNAC resources.
+# To insert new resources instead of updating, leave resource id blank.
+
+
+require "httparty"
+
+endpoint = "https://snac-dev.iath.virginia.edu/api/"
+api_key = "[INSERT API KEY]"
+
+CSV.foreach("./resources_for_update.csv", headers: true, header_converters: :symbol) do |row|
+# CSV with columns: id, type, title, display_entry, abstract, link, extent, date, repo_ic_id
+    request = {
+        "command": "insert_resource",
+        "resource": {
+            "dataType": "Resource",
+            "documentType": {
+                "id": row[:type]
+            },
+            "id": row[:id],
+            "title": row[:title],
+            "display_entry": row[:display_entry],
+            "abstract": row[:abstract],
+            "link": row[:link],
+            "extent": row[:extent],
+            "date": row[:date],
+            "repo_ic_id": row[:repo_ic_id],       #SNAC ID of the resource's holding repository
+            "operation": "insert"
+        },
+        "apikey": "#{api_key}"
+    }
+
+    response = HTTParty.put(endpoint, :body => request.to_json, :verify => false)
+
+    puts "Updated \n Resource id: #{response["resource"]["id"]} \n Resource title: #{response["resource"]["title"]}"
+end
